0886907ab86a9bd5dda130095b51c6a38b7735c7
Jan Sommer <soja-lists@aries.uberspace.de>
2018-01-30 23:30:04 +0100

    [embedded] Setup of timers finished

---
+++ examples/embedded/time_measurement.h
@@ -0,0 +1,13 @@
+#ifndef TIME_MEASUREMENT_H
+#define TIME_MEASUREMENT_H
+
+void
+time_measurement_init();
+
+void
+time_measurement_start();
+
+void
+time_measurement_stop();
+
+#endif // TIME_MEASUREMENT_H
