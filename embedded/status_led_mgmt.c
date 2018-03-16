33ab0ebde195d6ed879a8bc3fc01b9c7fd86e487
Jan Sommer <soja-lists@aries.uberspace.de>
2018-03-16 22:30:27 +0100

    [embedded] Fix status_led_mgmt
    Fixes due to debugging.

--- embedded/status_led_mgmt.c
+++ embedded/status_led_mgmt.c
@@ -5,7 +5,7 @@
 
 #include "time_measurement.h"
 
-#define MAX_TIMER_VALUE 0xFFFF;
+#define MAX_TIMER_VALUE 0xFFFF
 #define MAX_TOGGLE_TIME 0xFFFFFFFF
 #define MS_TO_TICKS(ms) (32768*ms/1000)
 
@@ -18,6 +18,8 @@ typedef struct
     uint32_t maximum_time_ticks;
 } pin_toggle_config_t;
 
+static const pin_toggle_config_t disabled = { 0, MAX_TIMER_VALUE+1, 0 };
+
 static pin_toggle_config_t m_pin_power_config;
 static pin_toggle_config_t m_pin_reset_config;
 static led_status_t m_status = LED_STATUS_NONE;
@@ -26,14 +28,14 @@ static led_status_t m_status = LED_STATUS_NONE;
 static void
 start_toggle(uint32_t pin_number)
 {
+    nrf_drv_timer_clear(&timer2);
     if (pin_number == PIN_POWER)
     {
-        m_pin_power_config.next_time = nrf_drv_timer_capture(&timer2, NRF_TIMER_CC_CHANNEL0)
-                                              + m_pin_power_config.toggle_time_ticks;
-        m_pin_power_config.next_time %= MAX_TIMER_VALUE;
+        m_pin_power_config.next_time = m_pin_power_config.toggle_time_ticks;
         nrf_timer_cc_write(timer2.p_reg, NRF_TIMER_CC_CHANNEL0,
                            m_pin_power_config.next_time);
-        nrf_timer_int_enable(timer2.p_reg, nrf_timer_compare_int_get(NRF_TIMER_CC_CHANNEL0));
+        nrf_drv_timer_compare_int_enable(&timer2, NRF_TIMER_CC_CHANNEL0);
+        nrf_gpio_pin_set(PIN_POWER);
     }
     else
     {
@@ -42,16 +44,19 @@ start_toggle(uint32_t pin_number)
         m_pin_reset_config.next_time %= MAX_TIMER_VALUE;
         nrf_timer_cc_write(timer2.p_reg, NRF_TIMER_CC_CHANNEL1,
                            m_pin_reset_config.next_time);
-        nrf_timer_int_enable(timer2.p_reg, nrf_timer_compare_int_get(NRF_TIMER_CC_CHANNEL1));
+        nrf_drv_timer_compare_int_enable(&timer2, NRF_TIMER_CC_CHANNEL1);
+        nrf_gpio_pin_set(PIN_RESET);
     }
 }
 
 static void
 stop_toggle()
 {
-    nrf_drv_timer_disable(&timer2);
-    nrf_timer_int_disable(timer2.p_reg, nrf_timer_compare_int_get(NRF_TIMER_CC_CHANNEL0));
-    nrf_timer_int_disable(timer2.p_reg, nrf_timer_compare_int_get(NRF_TIMER_CC_CHANNEL1));
+//    nrf_drv_timer_disable(&timer2);
+    nrf_drv_timer_compare_int_disable(&timer2, NRF_TIMER_CC_CHANNEL0);
+    nrf_drv_timer_compare_int_disable(&timer2, NRF_TIMER_CC_CHANNEL1);
+    m_pin_power_config = disabled;
+    m_pin_reset_config = disabled;
     nrf_gpio_pin_clear(PIN_POWER);
     nrf_gpio_pin_clear(PIN_RESET);
 }
@@ -60,33 +65,45 @@ static void
 timer2_int_handler(nrf_timer_event_t event_type,
                    void* p_context)
 {
+    static volatile int count = 0;
     if (event_type == NRF_TIMER_EVENT_COMPARE0)
     {
-        if (m_pin_power_config.maximum_time_ticks < m_pin_power_config.toggle_time_ticks)
+        if (m_pin_power_config.next_time < m_pin_power_config.maximum_time_ticks)
         {
             m_pin_power_config.next_time += m_pin_power_config.toggle_time_ticks;
+            volatile uint32_t nextTimer = m_pin_power_config.next_time & MAX_TIMER_VALUE;
             nrf_timer_cc_write(timer2.p_reg, NRF_TIMER_CC_CHANNEL0,
-                               m_pin_power_config.next_time);
+                               nextTimer/*m_pin_power_config.next_time & MAX_TIMER_VALUE*/);
+            nrf_gpio_pin_toggle(PIN_POWER);
+            count++;
         }
         else
         {
             stop_toggle();
-            m_status_callback(m_status);
+            if (m_status_callback)
+            {
+                m_status_callback(m_status);
+            }
             m_status = LED_STATUS_NONE;
         }
     }
     else
     {
-        if (m_pin_reset_config.maximum_time_ticks < m_pin_reset_config.toggle_time_ticks)
+        count = event_type + 10;
+        if (m_pin_reset_config.next_time < m_pin_reset_config.maximum_time_ticks)
         {
             m_pin_reset_config.next_time += m_pin_reset_config.toggle_time_ticks;
             nrf_timer_cc_write(timer2.p_reg, NRF_TIMER_CC_CHANNEL1,
-                               m_pin_reset_config.next_time);
+                               m_pin_reset_config.next_time & MAX_TIMER_VALUE);
+            nrf_gpio_pin_toggle(PIN_RESET);
         }
         else
         {
             stop_toggle();
-            m_status_callback(m_status);
+            if (m_status_callback)
+            {
+                m_status_callback(m_status);
+            }
             m_status = LED_STATUS_NONE;
         }
     }
@@ -114,8 +131,9 @@ status_led_mgmt_init(gpio_mgt_status_evt_handler_t callback)
 
     nrf_drv_timer_config_t timer_config = NRF_DRV_TIMER_DEFAULT_CONFIG;
     timer_config.mode = NRF_TIMER_MODE_COUNTER;
-    APP_ERROR_CHECK(nrf_drv_timer_init(&timer2, &timer_config, timer2_int_handler));
-
+    uint32_t err_code = nrf_drv_timer_init(&timer2, &timer_config, timer2_int_handler);
+    APP_ERROR_CHECK(err_code);
+    nrf_drv_timer_enable(&timer2);
     time_measurement_rtc_start();
 }
 
@@ -145,6 +163,7 @@ status_led_mgmt_set_status(led_status_t status)
         m_pin_reset_config.toggle_time_ticks  = MS_TO_TICKS(50);
         start_toggle(PIN_POWER);
         start_toggle(PIN_RESET);
+        break;
     case LED_STATUS_ACTIVE_MEASUREMENT:  // tick both leds alternating on every tick for 100ms
     {
         static uint32_t last_led = PIN_POWER;
@@ -171,6 +190,7 @@ status_led_mgmt_set_status(led_status_t status)
         m_pin_reset_config.toggle_time_ticks  = MS_TO_TICKS(1000);
         start_toggle(PIN_POWER);
         start_toggle(PIN_RESET);
+        break;
     case LED_STATUS_ERROR:               // both leds on
         nrf_gpio_pin_set(PIN_POWER);
         nrf_gpio_pin_set(PIN_RESET);
