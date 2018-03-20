9ccac704f10c5019d92819dc85a35221e754f70b
Jan Sommer <soja-lists@aries.uberspace.de>
2018-03-20 21:43:03 +0100

    [mobile] Add bluetooth device scanning

--- mobile/app/remoteconnection.cpp
+++ mobile/app/remoteconnection.cpp
@@ -2,17 +2,22 @@
 #include <iostream>
 
 RemoteConnection::RemoteConnection(QObject *parent) :
-    QObject(parent)
+    QObject(parent),
+    mDevice()
 {
+    connect(&mDiscoveryAgent, SIGNAL(deviceDiscovered(const QBluetoothDeviceInfo&)),
+              this, SLOT(newDevice(const QBluetoothDeviceInfo&)));
+    connect(&mDiscoveryAgent, SIGNAL(error(QBluetoothDeviceDiscoveryAgent::Error)),
+              this, SLOT(deviceScanError(QBluetoothDeviceDiscoveryAgent::Error)));
+    connect(&mDiscoveryAgent, SIGNAL(finished()), this, SLOT(deviceScanFinished()));
 }
 
 void
-RemoteConnection::connect()
+RemoteConnection::connectDevice()
 {
     if (mStatus != ConnectionStatus::Connected)
     {
-        // TODO: Implement
-        emit statusChanged(mStatus);
+        mDiscoveryAgent.start();
     }
 }
 
@@ -21,7 +26,9 @@ RemoteConnection::disconnect()
 {
     if (mStatus == ConnectionStatus::Connected || mStatus == ConnectionStatus::ActiveMeasurement)
     {
+        // TODO: Implement
         qDebug("Changed status to Disconnected");
+        mStatus = ConnectionStatus::Disconnected;
         emit statusChanged(mStatus);
     }
 }
@@ -40,4 +47,50 @@ void
 RemoteConnection::stopMeasurement()
 {
     // TODO: Implement
+}
+
+void
+RemoteConnection::newDevice(const QBluetoothDeviceInfo& info)
+{
+    if ((info.coreConfigurations() & QBluetoothDeviceInfo::LowEnergyCoreConfiguration)
+        && (info.name() == "Sommerzeit"))
+    {
+         mDevice = info;
+         qDebug("Found device %s", mDevice.name().toStdString().c_str());
+         mDiscoveryAgent.stop();
+         mStatus = ConnectionStatus::Connected;
+         emit statusChanged(mStatus);
+     }
+}
+
+void
+RemoteConnection::deviceScanError(QBluetoothDeviceDiscoveryAgent::Error error)
+{
+    if (error == QBluetoothDeviceDiscoveryAgent::PoweredOffError)
+    {
+        mStatus = ConnectionStatus::BluetoothDisabled;
+        emit statusChanged(mStatus);
+    }
+//    else if (error == QBluetoothDeviceDiscoveryAgent::InputOutputError)
+//        setUpdate("Writing or reading from the device resulted in an error.");
+//    else
+//        setUpdate("An unknown error has occurred.");
+
+//    m_deviceScanState = false;
+//    emit devicesUpdated();
+//    emit stateChanged();
+}
+
+void
+RemoteConnection::deviceScanFinished()
+{
+    if (mDevice.isValid())
+    {
+        mStatus = ConnectionStatus::Connected;
+    }
+    else
+    {
+        mStatus = ConnectionStatus::DeviceNotFound;
+    }
+    emit statusChanged(mStatus);
 }
